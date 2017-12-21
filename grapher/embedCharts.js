e60f4c98cee0ced421fa7a4e3607311ff4708f15
Jaiden Mispy <jaiden@mispy.me>
2017-12-21 09:23:08 +0800

    Static grapher

---
+++ grapher/embedCharts.js
@@ -0,0 +1,33 @@
+
+        window.App = {};
+        window.Global = { rootUrl: '/grapher' };
+
+        var link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.href = '/grapher/assets/charts.bundle.f80c8ed73c8649676b9c.css';
+        document.head.appendChild(link);
+
+        var hasPolyfill = false;
+        var hasGrapher = false;
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = function() {
+            hasPolyfill = true;
+            if (hasGrapher)
+                window.Grapher.embedAll();
+        }
+        script.src = "https://cdn.polyfill.io/v2/polyfill.min.js?features=es6,fetch"
+        document.head.appendChild(script);
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = function() {
+            hasGrapher = true;
+            if (hasPolyfill)
+                window.Grapher.embedAll();
+        }
+        script.src = '/grapher/assets/charts.bundle.f80c8ed73c8649676b9c.js';
+        document.head.appendChild(script);
+    
