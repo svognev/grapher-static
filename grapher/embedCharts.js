6f1468d1e879c0c396b3f0f4832783c5cf514eed
OWID <owid@ourworldindata.org>
2018-01-17 00:41:58 -0500

    Automated update

--- grapher/embedCharts.js
+++ grapher/embedCharts.js
@@ -5,7 +5,7 @@
         var link = document.createElement('link');
         link.type = 'text/css';
         link.rel = 'stylesheet';
-        link.href = '/grapher/assets/charts.bundle.edba348f087b6f8de0ee.css';
+        link.href = '/grapher/assets/charts.bundle.c2a6a73beb1eec1f8873.css';
         document.head.appendChild(link);
 
         var hasPolyfill = false;
@@ -28,6 +28,6 @@
             if (hasPolyfill)
                 window.Grapher.embedAll();
         }
-        script.src = '/grapher/assets/charts.bundle.edba348f087b6f8de0ee.js';
+        script.src = '/grapher/assets/charts.bundle.c2a6a73beb1eec1f8873.js';
         document.head.appendChild(script);
     
