3f9c2f43a670dfdd7281ade6a20474fe8205f848
OWID <owid@ourworldindata.org>
2018-03-06 02:25:37 -0500

    Automated update

--- grapher/embedCharts.js
+++ grapher/embedCharts.js
@@ -5,7 +5,7 @@
         var link = document.createElement('link');
         link.type = 'text/css';
         link.rel = 'stylesheet';
-        link.href = 'https://ourworldindata.org/grapher/assets/charts.css?v=charts.bundle.a5eb8f3e67b4d4e0b7cc.css';
+        link.href = 'https://ourworldindata.org/grapher/assets/charts.css?v=charts.bundle.b29bc839f4cd6d99c543.css';
         document.head.appendChild(link);
 
         var hasPolyfill = false;
@@ -28,6 +28,6 @@
             if (hasPolyfill)
                 window.Grapher.embedAll();
         }
-        script.src = 'https://ourworldindata.org/grapher/assets/charts.js?v=charts.bundle.a5eb8f3e67b4d4e0b7cc.js';
+        script.src = 'https://ourworldindata.org/grapher/assets/charts.js?v=charts.bundle.b29bc839f4cd6d99c543.js';
         document.head.appendChild(script);
     
