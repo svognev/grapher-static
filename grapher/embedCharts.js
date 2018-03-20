b74ef4e2bee9a89f4c030eebc130e3a639f52b6d
OWID <owid@ourworldindata.org>
2018-03-20 06:48:51 +0000

    Automated update

--- grapher/embedCharts.js
+++ grapher/embedCharts.js
@@ -5,29 +5,34 @@
         var link = document.createElement('link');
         link.type = 'text/css';
         link.rel = 'stylesheet';
-        link.href = 'https://ourworldindata.org/grapher/assets/charts.css?v=charts.bundle.bed4e47745c60119a429.css';
+        link.href = 'https://ourworldindata.org/grapher/assets/commons.css?v=commons.bundle.90c169ad51ecf04c4835.css';
         document.head.appendChild(link);
 
         var hasPolyfill = false;
         var hasGrapher = false;
 
+        var loadedScripts = 0;
+        function checkReady() {
+            if (loadedScripts == 3) {
+                window.Grapher.embedAll();
+            }
+        }
+
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.onload = function() {
-            hasPolyfill = true;
-            if (hasGrapher)
-                window.Grapher.embedAll();
-        }
+        script.onload = checkReady;
         script.src = "https://cdn.polyfill.io/v2/polyfill.min.js?features=es6,fetch"
         document.head.appendChild(script);
 
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.onload = function() {
-            hasGrapher = true;
-            if (hasPolyfill)
-                window.Grapher.embedAll();
-        }
-        script.src = 'https://ourworldindata.org/grapher/assets/charts.js?v=charts.bundle.bed4e47745c60119a429.js';
+        script.onload = checkReady;
+        script.src = 'https://ourworldindata.org/grapher/assets/commons.js?v=commons.bundle.90c169ad51ecf04c4835.js';
+        document.head.appendChild(script);
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = checkReady;
+        script.src = 'https://ourworldindata.org/grapher/assets/charts.js?v=charts.bundle.dcf65b68eddaba4c6a58.js';
         document.head.appendChild(script);
     
