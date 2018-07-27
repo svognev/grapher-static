c32aab9c257a2edf0810913a679f89ae0c4a633f
OWID <owid@ourworldindata.org>
2018-07-27 11:10:51 +0000

    Automated update

--- grapher/embedCharts.js
+++ grapher/embedCharts.js
@@ -5,7 +5,7 @@
         var link = document.createElement('link');
         link.type = 'text/css';
         link.rel = 'stylesheet';
-        link.href = 'https://ourworldindata.org/grapher/assets/commons.bundle.321952256b955dfc8c6f.css';
+        link.href = 'https://ourworldindata.org/grapher/assets/commons.bundle.b884d1cd972c8fba96fe.css';
         document.head.appendChild(link);
 
         var hasPolyfill = false;
@@ -28,12 +28,12 @@
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.onload = checkReady;
-        script.src = 'https://ourworldindata.org/grapher/assets/commons.bundle.321952256b955dfc8c6f.js';
+        script.src = 'https://ourworldindata.org/grapher/assets/commons.bundle.b884d1cd972c8fba96fe.js';
         document.head.appendChild(script);
 
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.onload = checkReady;
-        script.src = 'https://ourworldindata.org/grapher/assets/charts.bundle.321952256b955dfc8c6f.js';
+        script.src = 'https://ourworldindata.org/grapher/assets/charts.bundle.b884d1cd972c8fba96fe.js';
         document.head.appendChild(script);
     
